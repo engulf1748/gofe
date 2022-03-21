3d2af0fd17b3171990ee43be1ab4dbde04dea7f9
Tristan Brewster <platox@tutanota.com>
2022-03-21 11:23:29 -0600

    ui: updated Footer links
    I added a privacy policy link and a link to Infinium's website,
    as suggested.

--- public/components/Footer.tsx
+++ public/components/Footer.tsx
@@ -1,9 +1,11 @@
 import ExternalLink from "./ExternalLink";
 import InternalLink from "./InternalLink";
 
+import { icons } from '../data/icons';
+
 
 interface Link {
-	label: string;
+	label: string | React.ReactNode;
 	href?: string;
 	to?: string;
 }
@@ -12,9 +14,17 @@ interface Link {
 // to is for internal links.
 const links: Link[] = [
 	{
+		label: 'Privacy Policy',
+		to: '/privacy',
+	},
+	{
 		label: 'Source Code',
 		href: 'https://codeberg.org/ar324/gofe',
-	}
+	},
+	{
+		label: (<p className='flex align-c justify-c flex-row lh-1 fs-sm mb-0'>Backed with <i className="j-icon sm mx-0-25r"><span className="text-red-500">{icons.heart}</span></i> by Infinium</p>),
+		href: 'https://infinium.earth',
+	},
 ];
 
 const Footer = () => {
@@ -22,17 +32,28 @@ const Footer = () => {
 		<div className='footer'>
 			<div className="container h-100p">
 				<div className="flex align-c justify-c flex-row h-100p">
-					{links.map(link => (
-						link.to ? (
-							<InternalLink key={link.href} href={link.href || ''}>
-								<div className='link'>{link.label}</div>
-							</InternalLink>
-						) : (
-							<ExternalLink key={link.href} href={link.href || ''} className='link'>
-								{link.label}
-							</ExternalLink>
+					{links.map((link, i) => {
+						const LinkElem = () => (
+							link.to ? (
+								<InternalLink key={link.href} href={link.href || ''}>
+									<div className='link'>{link.label}</div>
+								</InternalLink>
+							) : (
+								<ExternalLink key={link.href} href={link.href || ''} className='link'>
+									{link.label}
+								</ExternalLink>
+							)
+						);
+
+						const isLast = i === links.length - 1;
+
+						return (
+							<>
+								<LinkElem />
+								{!isLast && <p className="mb-0 mx-1r text-dynamic-04">|</p>}
+							</>
 						)
-					))}
+					})}
 				</div>
 			</div>
 		</div>
--- public/data/icons.js
+++ public/data/icons.js
@@ -7,6 +7,11 @@ const icons = {
 			<path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
 		</svg>
 	),
+	heart: (
+		<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+			<path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+		</svg>
+	),
 }
 
 export {
