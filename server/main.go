19b8bd4954f10212dcf02b0a0c1c3d0089ebbbe3
Ajay R <ar324@protonmail.com>
2022-03-21 12:37:18 +0530

    server: add server prototype

---
+++ server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"codeberg.org/ar324/gofe/api"
+)
+
+func search(w http.ResponseWriter, r *http.Request) {
+	q, ok := r.URL.Query()["q"]
+	if !ok {
+		return
+	}
+	rs, err := api.Search(strings.Join(q, " "))
+	if err != nil {
+		return
+	}
+	d, err := json.Marshal(rs)
+	if err != nil {
+		return
+	}
+	fmt.Fprintf(w, string(d))
+}
+
+func main() {
+	port := os.Args[1]
+	http.HandleFunc("/search", search)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("localhost:%s", port), nil))
+}
