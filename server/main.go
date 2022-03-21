b421f4bf43156cd91f44623fe8f89a4c64629dc0
Ajay R <ar324@protonmail.com>
2022-03-21 20:19:57 +0530

    server: add suggest

--- server/main.go
+++ server/main.go
@@ -27,8 +27,25 @@ func search(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(d))
 }
 
+func suggest(w http.ResponseWriter, r *http.Request) {
+	q, ok := r.URL.Query()["q"]
+	if !ok {
+		return
+	}
+	rs, err := api.Suggest(strings.Join(q, " "))
+	if err != nil {
+		return
+	}
+	d, err := json.Marshal(rs)
+	if err != nil {
+		return
+	}
+	fmt.Fprintf(w, string(d))
+}
+
 func main() {
 	port := os.Args[1]
 	http.HandleFunc("/search", search)
+	http.HandleFunc("/suggest", suggest)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("localhost:%s", port), nil))
 }
