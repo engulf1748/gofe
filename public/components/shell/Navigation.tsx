5aac7de2cad3c9afcae2967073d0534fa57c9f39
Tristan Brewster <platox@tutanota.com>
2022-03-23 23:06:55 -0600

    public: Add dark/light mode toggle
    Along with some minor dark mode adjustments

--- public/components/shell/Navigation.tsx
+++ public/components/shell/Navigation.tsx
@@ -1,8 +1,22 @@
+import { useTheme } from "next-themes";
+import { icons } from "../../data/icons";
+
 import Logo from "../Logo";
 import SearchBar from "../search/SearchBar";
 
 
 const Navigation = () => {
+	const { theme, setTheme } = useTheme();
+
+	const onClick = () => {
+		// Since the default theme is system, we can't toggle
+		// that value directly. Instead, we have to see what theme
+		// the user currently has. The only way is to check the
+		// class output from the html element.
+		const isDark = document.querySelector('html')?.classList.contains('dark');
+		setTheme(isDark ? 'light' : 'dark');
+	}
+
 	return (
 		<nav>
 			<div className="wrapper flex-c">
@@ -12,6 +26,12 @@ const Navigation = () => {
 						<div className="w-100p mw-20r landscape-mw-none">
 							<SearchBar />
 						</div>
+					</div>
+					<div className="grid-block tablet-hide"></div>
+					<div className="grid-block align-e landscape-hide">
+						<button className='action' onClick={onClick}>
+							<i className="j-icon">{theme === 'dark' ? icons.sun : icons.moon}</i>
+						</button>
 					</div>
 				</div>
 			</div>
