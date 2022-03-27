e0057ed25467ef01d1bee25f3dc6d6ba558abf76
Ajay R <ar324@protonmail.com>
2022-03-27 08:35:38 +0530

    middleware: logging: replace tabs with spaces

--- middleware.go
+++ middleware.go
@@ -26,7 +26,7 @@ func newLogResponseWriter(w http.ResponseWriter) *logResponseWriter {
 
 func loggingHandler(out io.Writer, next http.Handler) http.Handler {
 	if out != nil {
-		logger = log.New(out, "\t", log.LstdFlags|log.LUTC|log.Lmsgprefix)
+		logger = log.New(out, "", log.LstdFlags|log.LUTC)
 	}
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		lw := newLogResponseWriter(w)
@@ -36,7 +36,13 @@ func loggingHandler(out io.Writer, next http.Handler) http.Handler {
 		if logger != nil {
 			// format: TIME PATH [Only the path component: we do not log the query parameters] RESULT-CODE
 			// log.LstdFlags takes care of date and time
-			logger.Printf("%s\t%d\n", r.URL.Path, lw.statusCode)
+			// 2022/03/27 03:03:24 /invalid 404
+			// 2022/03/27 03:03:33 /search 200
+			// 2022/03/27 03:03:38 /opensuggest 200
+			// 2022/03/27 03:03:44 /opensuggest 500
+			// 2022/03/27 03:03:50 /invliad/path 404
+			// 2022/03/27 03:04:00 /search 200
+			logger.Printf("%s %d\n", r.URL.Path, lw.statusCode)
 		}
 	})
 }
