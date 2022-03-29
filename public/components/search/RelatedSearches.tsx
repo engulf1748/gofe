495614849e097bed30e3cbfce9c872b33fac9448
Tristan B <tristan@infinium.earth>
2022-03-29 11:31:54 -0600

    public: Fix suggestions direct mutation issue
    We can't ever reassign state objects manually. That may appear to work, but we'll get some nasty issues later on. We must always work on a new reference of the data or use the React-given functional updaters.
    
    In this case, filter() and map() return a _new_ array, so it's fine.

--- public/components/search/RelatedSearches.tsx
+++ public/components/search/RelatedSearches.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
-import { useQuery } from "../../providers/QueryProvider"
+
+import { useQuery } from "../../providers/QueryProvider";
+
 
 const RelatedSearches = () => {
 	const { query, setQuery, suggestions } = useQuery();
@@ -14,15 +16,15 @@ const RelatedSearches = () => {
 		return <></>;
 	}
 
-	suggestions = suggestions.filter((suggestion: string) => suggestion !== query);
-
 	return (
 		(suggestions.length === 0) ? <></> : (
 			<div className="w-100p mt-3r">
 				<h4>Related searches</h4>
 
 				<div className="suggestion-pills">
-					{suggestions.map((suggestion: string) => {
+					{suggestions
+						.filter((suggestion: string) => suggestion !== query)
+						.map((suggestion: string) => {
 						return (
 							<div>
 								<button className="suggestion-pill" onClick={() => onSuggestionClick(suggestion)}>
