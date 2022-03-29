2ab7780e4376d2f51ab111138b5b27df3980c06b
Tristan B <tristan@infinium.earth>
2022-03-29 15:07:31 -0600

    public: Add viewport meta to _document

--- public/pages/_document.tsx
+++ public/pages/_document.tsx
@@ -6,12 +6,19 @@ const Document = () => {
 	return (
 		<Html>
 			<Head>
+				{/*
+					user-scalable=no prevents automatic zooming on mobile
+					devices after focusing the input.
+				*/}
+				<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+
 				<link rel="preconnect" href={config.api_domain} />
 				<link
 					rel="search"
 					type="application/opensearchdescription+xml"
 					title="Gofë Search"
-					href={`${config.api_domain}/opensearch.xml`}/>
+					href={`${config.api_domain}/opensearch.xml`}
+				/>
 			</Head>
 
 			<body>
