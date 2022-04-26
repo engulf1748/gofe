d096c25f4a5f20d8d5562faddf1876d5ebe09d1f
Ajay R <ar324@protonmail.com>
2022-04-26 10:13:33 +0530

    deploy: build before killing processes, name binary better

--- deploy.sh
+++ deploy.sh
@@ -14,17 +14,20 @@ deploy() {
 		return
 	fi
 	echo "Beginning deployment . . ."
-	killp
-	echo "Killed previous processes, if any . . ."
+	d="$(date --iso-8601=seconds)"
+	gd="gofe_$d"
+	go build -o "$gd" . || return
+	echo "API server build successful . . ."
 	cd public/
 	yarn next build || return
 	echo "public/ build successful . . ."
+	killp
+	echo "Killed previous build's processes, if any . . ."
 	yarn next start & # || return does not seem to work after '&'
-	echo "Front-end started . . ."
+	echo "Front-end server launched . . ."
 	cd ../
-	go build . || return
-	echo "Go server build successful . . ."
-	./gofe &
+	./"$gd" &
+	echo "API server launched . . ."
 	echo "Gofë has been deployed . . ."
 }
 
